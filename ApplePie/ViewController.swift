ae4d0466042bf298978976b8e4a87fcde98e8b4a
RushianHaker <ivanov2001m352g@mail.ru>
2018-04-05 16:37:56 +0300

    New logic

--- ApplePie/ViewController.swift
+++ ApplePie/ViewController.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var treeImgView: UIImageView!
+    
+    @IBOutlet weak var correctWordLable: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    var listOfWords = ["Otto", "Diana", "Maria"]
+    let incorrectMovesAllowed = 7
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        newRound()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    var currentGame: Game!
+    func newRound() {
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed)
+        updateUI();
+    }
+    
+    func updateUI() {
+        let name = "Tree \(currentGame.incorrectMovesRemaining)"
+        treeImgView.image = UIImage(named: name)
     }
 
     override func didReceiveMemoryWarning() {
